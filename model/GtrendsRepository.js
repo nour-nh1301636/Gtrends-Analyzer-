651a2876dd5074187c441a6a812090ac0d422fa8
nour201301636 <noor.m.92@hotmail.com>
2018-12-11 12:17:55 +0400

    40 records of inputs

--- model/GtrendsRepository.js
+++ model/GtrendsRepository.js
@@ -2,7 +2,6 @@
 
 class GtrendsRepository {
     constructor() {
-        //let googleTrends = require('./node_modules/google-trends-api/lib/google-trends-api.min.js');
         this.resultsOut=[];
         this.i=0;
     }
@@ -27,8 +26,7 @@ async loadDataFromJsonFiles()
              let value2;
              let value3;
              let resultJSON={};
-             // fout.exists("data/outputs.json", function(exists) {
-                 // if (exists) {
+
                       for  (let prop in timelineData)
                      {
                          time=timelineData[prop].time;
@@ -43,71 +41,20 @@ async loadDataFromJsonFiles()
                          resultJSON.value1=value1;
                          resultJSON.value2=value2;
                          resultJSON.value3=value3;
-                         console.log(resultJSON);
-                         this.resultsOut[this.i]=resultJSON;
-                     }
-             fout.appendFile("data/outputs.json", JSON.stringify(this.resultsOut[this.i]), (err) => {
+                        // console.log(JSON.stringify(resultJSON) + this.i);
+                        // this.resultsOut[this.i]=resultJSON;
+
+             fout.appendFile("data/outputs.json", JSON.stringify(resultJSON)+",", (err) => {
                  if (err) {
                      console.error(err);
                      return;
                  }
                  console.log("record saved");
-                 this.i=+1;
+
              });
-
-
+         }
+            // this.i=+1;
     }
-    async EditOutPut(){
-        const fs = require('fs-extra');
-        const data = await fs.readFile('data/outputs.json');
-        const outputs = JSON.parse(data);
-        console.log(outputs[0]);
-        let timeOutPuts=[];
-        let formattedTimeOutPuts=[];
-        let formattedAxisTimeOutPuts=[];
-        let CvalueOutPuts=[];
-        let RvalueOutPuts=[];
-        let SvalueOutPuts=[];
-        let ResultJSON = {};
-        let timelineData;
-        let prop;
-        let v=0;
-
-         timelineData=data.default.timelineData;
-         for  (prop in timelineData)
-         {
-             //v=0;
-             for(let time in timelineData[prop].time)
-                 timeOutPuts[v++]= time;
-             for(let formattedTime in timelineData[prop].formattedTime)
-                 formattedTimeOutPuts[v++]=formattedTime;
-             for(let formattedAxisTime in timelineData[prop].formattedAxisTime)
-                 formattedAxisTimeOutPuts[v++]=formattedAxisTime;
-             for(let value in timelineData[prop].value)
-             {
-                 CvalueOutPuts[v++]=value[0];
-                 RvalueOutPuts[v++]=value[1];
-                 SvalueOutPuts[v++]=value[2];
-             }
-
-         }
-
-         ResultJSON.Time=timeOutPuts;
-         ResultJSON. Formattedtime=formattedTimeOutPuts;
-         ResultJSON.FormattedAxisTime=formattedAxisTimeOutPuts;
-         RvalueOutPuts.CityValue=CvalueOutPuts;
-         RvalueOutPuts.RegionValue=RvalueOutPuts;
-         RvalueOutPuts.StateValue=SvalueOutPuts;
-             const fout=require('fs-extra');
-             let editedData=JSON.stringify(ResultJSON);
-             fout.appendFile("data/editedOutPuts.json",editedData,(err) => {
-                 if (err) {
-                     console.error(err);
-                     return;
-                 }
-                 console.log("File has been created");
-             });
-         }
 
 
 }
